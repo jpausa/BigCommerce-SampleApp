28bda4b0fbd50968799b749cda32010aafb3d156
Jorge <glph8911@gmail.com>
2023-05-26 12:38:42 -0500

    creating init files and installing big commerce node app

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import { Panel, Text } from "@bigcommerce/big-design";
+import React from "react";
+
+const Index = () => (
+  <Panel header="Homepage" margin="xxLarge">
+    <Text>Hello world</Text>
+  </Panel>
+);
+
+export default Index;
